aa7b9115187ebfb956743d6431c6c14ab7e196f2
Aksel Gregersen <akhegr@users.noreply.github.com>
2019-05-10 10:17:09 +0200

    The loop

---
+++ Loop1To100/Loop1To100/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loop1To100
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(Looper(i));
+            }
+            Console.ReadKey();
+        }
+
+        static string Looper(int number)
+        {
+            string val = "";
+            if (number % 3 == 0)
+            {
+                val += ("Aksel");
+            }
+
+            if (number % 5 == 0)
+            {
+                val += ("Gregersen");
+            }
+
+            if (val == "")
+            {
+                val += number;
+            }
+
+            return val;
+        }
+    }
+}
